4638dcec6ecdda3f5eddd672aa7282ba50cab9d6
Julia <julia@example.com>
2026-09-14 16:51:16 -0400

    Change shared access passcode

--- dist/access.js
+++ dist/access.js
@@ -22,7 +22,7 @@ if (localStorage.getItem(accessKey) === 'true') {
 
 accessForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (accessPasscode.value.trim() === 'FA_Julia') {
+  if (accessPasscode.value.trim() === 'julia') {
     localStorage.setItem(accessKey, 'true');
     openPlan();
     return;
